32ab7c953d599a67df20f77db2f13dcd2db5265b
sasaky <athene609.cyl@gmail.com>
2025-12-12 02:54:40 +0900

    「ディジタル回路の基礎：MUXと三項演算子」

--- src/Basic.v
+++ src/Basic.v
@@ -8,24 +8,47 @@ module Basic;
 
 	reg r_a;
 	reg r_b;
+	reg r_s;
 	// --- おまじないここまで ---
 
 	wire w_x;
-	assign w_x = r_a ^ r_b;
+	// マルチプレクサ（MUX）
+	// assign w_x = (r_a & ~r_s) | (r_b & r_s);
+	assign w_x = r_s ? r_b : r_a;
 
 	// --- おまじないここから ---
 	initial begin
 		r_a = 1'b0;
 		r_b = 1'b0;
+		r_s = 1'b0;
 		#2
 		r_a = 1'b1;
 		r_b = 1'b0;
+		r_s = 1'b0;
 		#2
 		r_a = 1'b0;
 		r_b = 1'b1;
+		r_s = 1'b0;
 		#2
 		r_a = 1'b1;
 		r_b = 1'b1;
+		r_s = 1'b0;
+		#2
+		r_a = 1'b0;
+		r_b = 1'b0;
+		r_s = 1'b1;
+		#2
+		r_a = 1'b1;
+		r_b = 1'b0;
+		r_s = 1'b1;
+		#2
+		r_a = 1'b0;
+		r_b = 1'b1;
+		r_s = 1'b1;
+		#2
+		r_a = 1'b1;
+		r_b = 1'b1;
+		r_s = 1'b1;
 		#2
 		$finish;
 	end
