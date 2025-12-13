e962fb3476ea6c67aed363fb1fb097a18f808aea
sasaky <athene609.cyl@gmail.com>
2025-12-14 05:42:53 +0900

    「練習問題：1」

---
+++ src/exam/probrem1.v
@@ -0,0 +1,20 @@
+
+/*
+ *  i_p0[7:0] ─┐
+ *             AND ── (w_p0[7:0]) ─┐
+ *  i_p1[7:0] ─┘                   OR ───── o_p[7:0]
+ *  i_p2[7:0] ─────────────────────┘
+ */
+module problem1(
+	input  [7:0] i_p0,
+	input  [7:0 ]i_p1,
+	input  [7:0] i_p2,
+	output [7:0] o_p
+);
+
+	wire [7:0] w_p0;
+
+	assign w_p0 = i_p0 & i_p1;
+	assign o_p  = i_p2 | w_p0;
+
+endmodule
